a08896ea6a87931a50f659c9bdb2990df7a4d5ad
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-10-04 17:39:52 +0200

    Order products

--- js/filter.js
+++ js/filter.js
@@ -2,10 +2,63 @@
 
 console.log('filter.js conectado')
 
-function ordenarName(){
-    console.log('alfabeticamente')
-    const listContainer = document.getElementById('listContainer');
-    const products = Array.from(listContainer.getElementsByTagName('li'))
 
-    console.log(products)
+// Ordenar por nombre
+function ordenarName() {
+    // Seleccionar el contenedor de la lista
+let ul = document.getElementById('listContainer');
+
+// Seleccionar todos los <li> dentro del <ul>
+let liElements = Array.from(ul.getElementsByTagName('li'));
+
+// Ordenar los <li> basados en el texto del <span> con clase .nombreSpan
+liElements.sort((a, b) => {
+    let nameA = a.querySelector('.nombreProducto').innerText.toLowerCase();
+    let nameB = b.querySelector('.nombreProducto').innerText.toLowerCase();
+    
+    // Comparar los nombres alfabéticamente
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+});
+
+// Limpiar la lista <ul>
+ul.innerHTML = '';
+
+// Reinsertar los <li> ordenados
+liElements.forEach(li => ul.appendChild(li));
+
+}
+
+
+//Ordenar por precio
+function ordenarPrecio() {
+        // Seleccionar el contenedor de la lista
+let ul = document.getElementById('listContainer');
+
+// Seleccionar todos los <li> dentro del <ul>
+let liElements = Array.from(ul.getElementsByTagName('li'));
+
+// Ordenar los <li> basados en el texto del <span> con clase .nombreSpan
+liElements.sort((a, b) => {
+    let nameA = a.querySelector('.precioProducto').innerText.toLowerCase();
+    let nameB = b.querySelector('.precioProducto').innerText.toLowerCase();
+    
+    // Comparar los nombres alfabéticamente
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+});
+
+// Limpiar la lista <ul>
+ul.innerHTML = '';
+
+// Reinsertar los <li> ordenados
+liElements.forEach(li => ul.appendChild(li));
+}
+
+
+//Ordenar por fecha (no ordenar)
+function sinOrdenar() {
+    document.getElementById('listContainer').innerHTML = localStorage.getItem('list')   
 }
